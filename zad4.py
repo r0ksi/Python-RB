dfa2daf5360e2ba483928b90ebf4b797a3cf20a0
r0ksi <61803292+r0ksi@users.noreply.github.com>
2022-12-05 21:47:35 +0100

    Delete zad4.py

--- zad4.py
+++
@@ -1,7 +0,0 @@
-list_of_strings = ['P', 'y', 't', 'h', 'o', 'n']
-quotes = ''
-for elem in list_of_strings:
-    quotes = quotes+ '' +elem
-# usuwanie ''
-if __name__ == '__main__':
-    print(quotes)
